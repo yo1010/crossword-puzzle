af332d5da1f8f6c40cb92c93ab6593a4503c871d
yavord10 <yavor.v.dimitrov@gmail.com>
2021-02-28 22:01:46 +0800

    fix vetical display bug + delete old code

--- src/App.js
+++ src/App.js
@@ -4,60 +4,103 @@ import { CHAR_WIDTH, START_AXES, WORDS } from './constants/constants';
 
 import './App.css';
 
+//NEED TO HANDLE WORDS THAT DON'T LINK
+
+//NEED TO HANDLE OVERLAPPING
+
 const App = () => {
   const [isGenerated, generateCrossword] = useState(false);
 
+  const handleToggle = () => {
+    if (isGenerated) {
+      generateCrossword(false);
+    } else {
+      generateCrossword(true);
+    }
+  };
+
+  //GENERATE CROSSWORD
   const renderWordsOntoCrosswords = (words) => {
     let isVertical = true;
     let displayedWords = [];
     let startY = START_AXES;
     let startX = START_AXES;
 
+    const updateUnavailableCharIndexes = (intersectionProps) => {
+      displayedWords[intersectionProps.displayedWordIndex] = {
+        ...displayedWords[intersectionProps.displayedWordIndex],
+        unavailableCharIndexes: [
+          ...displayedWords[intersectionProps.displayedWordIndex].unavailableCharIndexes,
+          intersectionProps.displayedCharIndex
+        ]
+      }
+    };
+
     //DETERMINE THE NEXT X, Y AXES TO DISPLAY WORD
-    const returnDisplayProperties = ({ oldWordCharIndex, newCharIndex }, displayedWord) => {
+    const returnDisplayProperties = ({ displayedCharIndex, newCharIndex }, displayedWord) => {
       const { isVertical } = displayedWord;
 
       const newCharIndexPixels = newCharIndex * CHAR_WIDTH;
-      const oldCharIndexPixels = oldWordCharIndex * CHAR_WIDTH;
+      const oldCharIndexPixels = displayedCharIndex * CHAR_WIDTH;
       console.log(oldCharIndexPixels, newCharIndexPixels)
 
       return {
         isVertical: isVertical ? false : true,
-        startY: isVertical ? displayedWord.startY + oldCharIndexPixels : displayedWord.startY + newCharIndexPixels,
-        startX: isVertical ? displayedWord.startX - newCharIndexPixels : displayedWord.startX - oldCharIndexPixels
+        startY: isVertical ? displayedWord.startY + oldCharIndexPixels : displayedWord.startY - newCharIndexPixels,
+        startX: isVertical ? displayedWord.startX - newCharIndexPixels : displayedWord.startX + oldCharIndexPixels
       }
     };
 
     //FIND THE INDEX OF THE WORD TO INTERSECT ---> FIND THE INDEXES OF CHARACTERS THAT INTERSECT
     const returnIntersectionProperties = (word) => {
       let newCharIndex;
-      let oldWordCharIndex;
+      let displayedCharIndex;
 
       const wordChars = displayedWords.map((item) => [...item.word]);
       console.log(wordChars);
 
       //DETERMINE INTERSECTION INDEXES OF WORD AND CHARACTERS
-      const newWordIndex = displayedWords
-      .map((item) => [...item.word])
-      .findIndex((charArray) => {
-        const isCharacterInWord = charArray.some((char, index) => {
-          if (word.includes(char)) {
-            console.log('-->', charArray, char, word);
-            oldWordCharIndex = index;
-            newCharIndex = word.indexOf(char);
+      const displayedWordIndex = displayedWords
+        .map((item) => {
+          //MAP ITEMS AND HANDLE ALREADY INTERSECTED CHARACTERS
+          console.log(item)
+          const { unavailableCharIndexes, word } = item;
+          if (unavailableCharIndexes.length > 0) {
+            const availableChars = [...word].map((char, index) => {
+              if (unavailableCharIndexes.includes(index)) {
+                return null;
+              }
+
+              return char;
+            });
+
+            return availableChars;
           }
 
-          return word.includes(char);
+          return [...word]
+        })
+        .findIndex((charArray) => {
+          //FIND DISPLAYED WORD TO INTERSECT WITH AND THE CHARACTERS THAT INTERSECT
+          console.log(charArray)
+          const isCharacterInWord = charArray.some((char, index) => {
+            if (word.includes(char)) {
+              console.log('-->', charArray, char, word);
+              displayedCharIndex = index;
+              newCharIndex = word.indexOf(char);
+            }
+
+            return word.includes(char);
+          });
+
+          return isCharacterInWord;
         });
 
-        return isCharacterInWord;
-      });
-      console.log(newWordIndex);
+      console.log(displayedWordIndex);
 
       return {
-        newWordIndex: newWordIndex,
+        displayedWordIndex: displayedWordIndex,
         newCharIndex: newCharIndex,
-        oldWordCharIndex: oldWordCharIndex
+        displayedCharIndex: displayedCharIndex
       };
     };
 
@@ -65,23 +108,35 @@ const App = () => {
     const mapWordsOntoCrosswords = (word, index) => {
       if (displayedWords.length === 0) {
         //DISPLAY FIRST WORD
-        displayedWords = [...displayedWords, { word: word, isVertical: isVertical, startY: startY, startX: startX }];
-      } else {
-        const intersectionProps = returnIntersectionProperties(word);
-        const displayProps = returnDisplayProperties(intersectionProps, displayedWords[intersectionProps.newWordIndex]);
         displayedWords = [...displayedWords, {
-          word: word, 
+          word: word,
+          isVertical: isVertical,
+          startY: startY,
+          startX: startX,
+          unavailableCharIndexes: [] 
+        }];
+      } else {
+        //DETERMINE INTERSECTION + DISPLAY PROPS FOR NEXT ITEMS
+        const intersectionProps = returnIntersectionProperties(word);
+        console.log('map next details', intersectionProps, word, displayedWords[intersectionProps.displayedWordIndex])
+        const displayProps = returnDisplayProperties(intersectionProps, displayedWords[intersectionProps.displayedWordIndex]);
+
+        displayedWords = [...displayedWords, {
+          word: word,
+          unavailableCharIndexes: [intersectionProps.newCharIndex],
           ...displayProps
         }];
+
+        updateUnavailableCharIndexes(intersectionProps);
+
         startY = `${displayProps.startY}px`;
         startX = `${displayProps.startX}px`;
-        //find word that can intersect with displayed word
-        isVertical = false;
+        isVertical = displayProps.isVertical;
       };
 
-      console.log(startX);
-      console.log(startY)
-  
+      console.log('y', startY);
+      console.log('x', startX);
+
       return (
         <Word
           startY={startY}
@@ -98,18 +153,18 @@ const App = () => {
 
   return (
     <div className="App">
-      <button onClick={() => generateCrossword(true)}>LINK WORDS</button>
+      <button onClick={handleToggle} className="link-button">LINK WORDS</button>
       {!isGenerated && 
         <div style={{ width: 'fit-content', margin: 'auto' }}>
           {WORDS.map((word, index) => (
-            <Word word={word} isVertical={index % 2 === 0} />
+            <Word word={word} isVertical={false} startY={index * CHAR_WIDTH} />
           ))}
         </div>
       }
 
       {isGenerated && 
         <div style={{ width: 'fit-content', margin: 'auto', height: '500px' }}>
-          {renderWordsOntoCrosswords(WORDS.slice(0, 4))}
+          {renderWordsOntoCrosswords(WORDS)}
         </div>
       }
     </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const WORDS = ['hello', 'mouse', 'country', 'octopus', 'pie', 'school', 'zero'];
+export const WORDS = ['hello', 'mouse', 'country', 'octopus', 'pie', 'school', 'zero', 'america'];
 
 export const CHAR_WIDTH = 20;
 
